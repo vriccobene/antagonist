533e04e5cec3dd200e0940c824698a00f40e9ab9
Vincenzo Riccobene <vincenzo.riccobene@gmail.com>
2024-07-21 22:34:39 +0200

    Add the ML lifecycle validation

--- frontend/layouts/index.py
+++ frontend/layouts/index.py
@@ -13,11 +13,11 @@ layout = html.Div(
     [
         dcc.Location(id="url", refresh=False),
         html.Div(id="page-name", children=["index"], style={"display": "none"}),
-        dcc.Interval(
-            id='interval-component',
-            interval=5000,  # Every 5 seconds
-            n_intervals=0
-        ),
+        # dcc.Interval(
+        #     id='interval-component',
+        #     interval=5000,  # Every 5 seconds
+        #     n_intervals=0
+        # ),
         html.Div(
             id="page-content",
             children=[
@@ -551,12 +551,12 @@ layout = html.Div(
 
 
 
-@service.app.callback(
-    Output("network-anomaly-table", "rowData"),
-    Input("interval-component", "n_intervals"),
-)
-def network_anomaly_table_update(n_intervals):
-    return network_anomaly_api.get_network_anomalies()
+# @service.app.callback(
+#     Output("network-anomaly-table", "rowData"),
+#     Input("interval-component", "n_intervals"),
+# )
+# def network_anomaly_table_update(n_intervals):
+#     return network_anomaly_api.get_network_anomalies()
 
 @service.app.callback(
     Output("network-anomaly-visualize-button", "style"),
